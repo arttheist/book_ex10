7532f1e7808e4ea9079efde8b3fc41f204c496ab
Rene Vos <re.vos@arttheisten.de>
2014-01-23 21:30:55 +0100

    Starting all over again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Book::Application.config.secret_token = '692ebd480ffbbf6ed59faef28c2d251f8a7a0618caea67b61bf1b52083626e3908d67e718fddb4d9b98ba104bb8bcbb5308941ed5cf41d4f4dd965b944da2b2e'
